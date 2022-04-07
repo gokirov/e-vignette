2f75218b1cff9959157306523a9b550f4a9c0ff3
Goran Kirov <goran.kirov@hotmail.com>
2022-04-08 00:05:59 +0200

    Added quarkus microservice for toll-validating

---
+++ microservices/Validity/validate-quarkus/src/test/java/si/modern/toll/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package si.modern.toll;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
